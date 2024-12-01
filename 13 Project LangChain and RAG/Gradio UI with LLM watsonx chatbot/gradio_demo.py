487307e15a032b8f4105f8616b45eab081608d1d
Zac <126036148+UndefinedNaCl@users.noreply.github.com>
2024-12-01 17:51:12 +0800

    Add files via upload

---
+++ 13 Project LangChain and RAG/Gradio UI with LLM watsonx chatbot/gradio_demo.py
@@ -0,0 +1,26 @@
+import gradio as gr
+
+# def add_numbers(Num1, Num2):
+#     return Num1 + Num2
+
+# # Define the interface
+# demo = gr.Interface(
+#     fn=add_numbers, 
+#     inputs=[gr.Number(), gr.Number()], # Create two numerical input fields where users can enter numbers
+#     outputs=gr.Number() # Create numerical output fields
+# )
+
+def combine(a,b):
+    return a + " " + b
+
+demo = gr.Interface(
+    fn=combine,
+    inputs = [
+        gr.Textbox(label="Sentence 1"),
+        gr.Textbox(label="Sentence 2")
+    ],
+    outputs = gr.Textbox(value = " ",label="Output")
+)
+
+# Launch the interface
+demo.launch(server_name="127.0.0.1", server_port= 7860)
